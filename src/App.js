1df88b80785d3fda09bb1e63787833b9501847c1
Christian Montoya <christian.montoya@consensys.net>
2022-08-31 12:27:51 -0400

    chublins.com configured

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { WagmiConfig, createClient } from "wagmi";
+import { ConnectKitProvider, ConnectKitButton, getDefaultClient } from "connectkit";
+
+const infuraId = process.env.INFURA_ID; 
+
+const client = createClient(
+  getDefaultClient({
+    appName: "Chublins",
+    infuraId: infuraId
+  }), 
+); 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WagmiConfig client={client}>
+      <ConnectKitProvider theme="rounded" options={{embedGoogleFonts: true,}}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Website under construction!
+            </p>
+            <ConnectKitButton />
+          </header>
+        </div>
+      </ConnectKitProvider>
+    </WagmiConfig>
   );
 }
 
