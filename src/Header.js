3ccb3d1c756f8d29c22c4b2dc010758abd7d74ae
Montoya <sirokai@gmail.com>
2022-09-03 22:24:05 -0400

    Temporary downtime

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom"; 
+// import { NavLink } from "react-router-dom"; 
 import hexagon from './hexagon.svg';
 
 // removed: <NavLink to="whitepaper">Whitepaper</NavLink>
@@ -8,13 +8,17 @@ const Header = () => {
       <div>
         <img src={hexagon} className="Chublin-logo" alt="logo" /> 
         <h1>Chublins</h1>
-        <NavLink to="/">Mint</NavLink>
-        <NavLink to="view">Viewer</NavLink>
-        <NavLink to="guide">Guide</NavLink>
         <a href="https://twitter.com/ChublinsNFT">Twitter</a>
       </div>
     </nav>
   ); 
 }; 
+
+/*
+
+        <NavLink to="/">Mint</NavLink>
+        <NavLink to="view">Viewer</NavLink>
+        <NavLink to="guide">Guide</NavLink>
+*/
 
 export default Header; 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
+import Update from './routes/Update'; /*
 import Guide from './routes/Guide'; 
 import Whitepaper from './routes/Whitepaper'; 
 import Mint from './routes/Mint'; 
 import Soldout from './routes/Soldout';
 import Secret from './routes/Secret';
 import View from './routes/View'; 
-import Token from './routes/Token'; 
+import Token from './routes/Token'; */
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,17 +19,21 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route index element={<Soldout />} />
-          <Route path="guide" element={<Guide />} />
-          <Route path="view" element={<View />}>
-            <Route path=":tokenId" element={<Token />} />
-          </Route>
-          <Route path="whitepaper" element={<Whitepaper />} />
+          <Route index element={<Update />} />
         </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
+
+/*
+
+          <Route path="guide" element={<Guide />} />
+          <Route path="view" element={<View />}>
+            <Route path=":tokenId" element={<Token />} />
+          </Route>
+          <Route path="whitepaper" element={<Whitepaper />} />
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
