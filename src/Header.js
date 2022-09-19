6227ba6c1cebcca83512caf00f583321bf77f443
Montoya <sirokai@gmail.com>
2022-09-19 11:05:00 -0400

    opensea

--- src/Header.js
+++ src/Header.js
@@ -11,6 +11,7 @@ const Header = () => {
         <NavLink to="/">Home</NavLink>
         <NavLink to="/mint">Mint</NavLink>
         <NavLink to="guide">Guide</NavLink>
+        <a href="https://opensea.io/collection/chublins-reborn">OpenSea</a>
         <a href="https://twitter.com/ChublinsNFT">Twitter</a>
       </div>
     </nav>
--- src/routes/Soldout.jsx
+++ src/routes/Soldout.jsx
@@ -3,7 +3,7 @@ export default function Soldout() {
     <div class="Page">
       <h2>Sold out!</h2>
       <p>The Chublins mint is now complete! 
-        You can use the <a href="/view">Chublins Viewer</a> to view each Chublin and also find the collection on <a href="https://opensea.io/collection/chublins-nft">OpenSea</a> and <a href="https://looksrare.org/collections/0x87f01cE90A531F496bA2D6557D0348c5447895CE">LooksRare</a>.</p>
+        You can use the <a href="/view">Chublins Viewer</a> to view each Chublin and also find the collection on <a href="https://opensea.io/collection/chublins-reborn">OpenSea</a> and <a href="https://looksrare.org/collections/0x87f01cE90A531F496bA2D6557D0348c5447895CE">LooksRare</a>.</p>
     </div>
   )
 }; 
