147b2e36759338161ba09401afe031b1129b4cce
Christian Montoya <christian.montoya@consensys.net>
2023-03-21 13:28:55 -0400

    Update Soldout.jsx

--- src/routes/Soldout.jsx
+++ src/routes/Soldout.jsx
@@ -9,9 +9,12 @@ export default function Soldout() {
       }
     }, []),
     <div className="Page">
-      <h2>Sold out!</h2>
-      <p>The Chublins mint is now complete! 
-        You can use the <a href="/view">Chublins Viewer</a> to view each Chublin and also find the collection on <a href="https://opensea.io/collection/chublins-reborn">OpenSea</a> and <a href="https://looksrare.org/collections/0x87f01cE90A531F496bA2D6557D0348c5447895CE">LooksRare</a>.</p>
+      <h2>Welcome</h2>
+      <p>Chublins is an on-chain art project. 443 Chublins are permanently immortalized on the Ethereum blockchain.</p>
+      <p>You can use the <a href="/view">Chublins Viewer</a> to view each Chublin and also find the collection on <a href="https://opensea.io/collection/chublins-reborn">OpenSea</a> and <a href="https://looksrare.org/collections/0x87f01cE90A531F496bA2D6557D0348c5447895CE">LooksRare</a>.</p>
+      <p>You can also learn more about Chublins by reading the <a href="/guide">guide</a>.</p>
+      <p>&nbsp;</p>
+      <p><em>Chublins are forever.</em></p>
     </div>
   )
 }; 
